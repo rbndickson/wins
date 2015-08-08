f8579e5aa246e4a70ef1d0fa4d50b195c3422206
Robin Dickson <robindickson@gmail.com>
2015-08-09 00:17:20 +0200

    Moved login to its own page

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Wins::Application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-  root to: 'wins#index'
+  root to: 'sessions#new'
   #   get 'products/:id' => 'catalog#view'
-  get 'register', to: 'users#new'
+  get '/register', to: 'users#new'
   get '/login', to: 'sessions#new' #login_path
   post '/login', to: 'sessions#create'
   get '/logout', to: 'sessions#destroy' #logout_path
