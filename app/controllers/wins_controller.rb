806f48f930c7982ecafd353d639108ab178ecfbf
Robin Dickson <robindickson@gmail.com>
2015-08-08 15:52:20 +0200

    Started routes and controllers for users and wins

---
+++ app/controllers/wins_controller.rb
@@ -0,0 +1,27 @@
+class WinsController < ApplicationController
+
+  def index
+    @wins = Win.all
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def show
+
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,17 +2,20 @@ Wins::Application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
-
-  # Example of regular route:
+  root to: 'wins#index'
   #   get 'products/:id' => 'catalog#view'
+  get 'register', to: 'users#new'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
+
+
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
+
+  resources :users, only: [:create, :edit, :update, :show]
+  resources :wins
 
   # Example resource route with options:
   #   resources :products do
@@ -39,7 +42,7 @@ Wins::Application.routes.draw do
   #       get 'recent', on: :collection
   #     end
   #   end
-  
+
   # Example resource route with concerns:
   #   concern :toggleable do
   #     post 'toggle'
