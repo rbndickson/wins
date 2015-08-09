a34182d62371dd6ad218dca377993136fe98bec1
Robin Dickson <robindickson@gmail.com>
2015-08-09 16:21:03 +0200

    Hid entry box when 3 wins have been entered

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  helper_method :current_user, :logged_in?
+  helper_method :current_user, :logged_in?, :current_win_count
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
@@ -10,6 +10,10 @@ class ApplicationController < ActionController::Base
 
   def logged_in?
     !!current_user
+  end
+
+  def current_win_count
+    Win.where(user_id: current_user).where("created_at >= ?", Time.zone.now.beginning_of_day).size
   end
 
 end
